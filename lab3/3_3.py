e1a5555e8f4b3535909511e6db41bdb6e1fd27dc
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-10-26 14:29:36 +0300

    Update 3_3.py

--- lab3/3_3.py
+++ lab3/3_3.py
@@ -2,10 +2,12 @@ import math
 x1,x2 = map(int,input("введите x1 и x2: ").split())
 N,m = map(int,input("введите N и m: ").split())
 h = (x2-x1)/m
+print(h)
 while x2 >= x1:
     s_x = 0
-    for i in range(N):
+    for i in range(N + 1):
         s_x += ((-1) ** i) * ((x1 ** (2 * i)) / math.factorial(2 * i))
     print(format(x1,'.3f'),' - ',format(s_x,'.3f'))
     x1+=h
         
+# протетсировать это дело 

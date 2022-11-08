91bbbd6d36d0c222ca3634c91c4b5a2eb44c0106
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-11-08 17:25:32 +0300

    Create 1_1.py

---
+++ lab5/1_1.py
@@ -0,0 +1,27 @@
+from random import randint
+
+N = randint(3, 10) # столбцы
+M = randint(3, 10) # строки
+A = [[0]*M for i in range(N)]
+
+print(N, M)
+for i in range(N):
+    for j in range(M):
+        A[i][j] = randint(-20, 20)
+
+for i in range(N):
+    summ = 0
+    count = 0
+    middle = 0
+    for j in range(M):
+        if A[i][j] > 0:
+            summ += A[i][j]
+            count += 1
+    print(f"Для строки {i} сумма = {summ} количество = {count} среднее арифметическое = {summ/count}")
+
+
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        print(A[i][j], end=' ')
+    print()
+

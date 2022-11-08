524a3123f0059878304854c62185a2160327d919
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-11-08 21:59:39 +0300

    Create 3_2.py

---
+++ lab5/3_2.py
@@ -0,0 +1,33 @@
+from random import randint
+
+N = randint(7, 10) # столбцы
+A = [[0]*N for i in range(N)]
+for i in range(N):
+    for j in range(N):
+        A[i][j] = randint(-20, 20)
+
+tmp = []
+
+for i in range(N):
+    for j in range(N):
+        if j < i:
+            tmp.append(A[i][j])
+
+index = 0
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        print("\t%4d" % (A[i][j]), end=' ')
+    print()
+print("-------------------------------------")
+tmp.sort(reverse=True)
+for i in range(N):
+    for j in range(N):
+        if j < i:
+            A[i][j] = tmp[index]
+            index += 1
+
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        print("\t%4d" % (A[i][j]), end=' ')
+    print()
+
